19a8fdd1125cf84e99bdd251a8788abade355d15
Panayiotis Georgiou <panayiotisgeorgiou87@gmail.com>
2016-12-21 10:16:23 +0000

    create ad-sense file

---
+++ ad-sense-code.js
@@ -0,0 +1,10 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" 
+onerror="adBlockFunction();">
+</script>
+<ins class="adsbygoogle"
+style="display:inline-block;width:300px;height:600px"
+data-ad-client="ca-pub-XXXXXXXXXXXXX"
+data-ad-slot="XXXXXXX"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
