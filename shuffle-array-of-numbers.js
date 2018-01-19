e84bc079e048e5e2b20d9505aff67edc8a08022c
Panayiotis Georgiou <georgioupanayiotis@users.noreply.github.com>
2018-01-19 13:00:40 +0000

    create shuffle-array-of-numbers.js

---
+++ shuffle-array-of-numbers.js
@@ -0,0 +1,3 @@
+var numbers = [5, 458 , 120 , -215 , 228 , 400 , 122205, -85411];
+numbers = numbers.sort(function(){ return Math.random() - 0.5});
+/* the array numbers will be equal for example to [120, 5, 228, -215, 400, 458, -85411, 122205]  */
